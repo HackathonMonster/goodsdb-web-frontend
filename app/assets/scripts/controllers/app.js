eefa1180ebba6503b18a7f0e461c1d053fdc3fb0
Daniel Perez <daniel@claudetech.com>
2014-12-14 16:31:14 +0900

    Improve display and add search by tag.

--- app/assets/scripts/controllers/app.js
+++ app/assets/scripts/controllers/app.js
@@ -1,9 +1,25 @@
 function AppCtrl($scope, $state, userManager) {
   $scope.search = '';
+  $scope.previousSearch = '';
 
-  $scope.filter = function () {
+  $scope.filter = function ($event) {
+    $event.target.search.blur();
     $scope.$broadcast('filter', $scope.search);
+  };
+
+  $scope.$on('filter', function (e, tag) {
+    $scope.search = tag;
+    $scope.previousSearch = $scope.search;
+  });
+
+  $scope.clear = function () {
     $scope.search = '';
+  };
+
+  $scope.restore = function () {
+    if ($scope.search === '') {
+      $scope.search = $scope.previousSearch;
+    }
   };
 
   $scope.logout = function () {
--- app/assets/scripts/directives/items/brick.js
+++ app/assets/scripts/directives/items/brick.js
@@ -1,10 +1,17 @@
+var itemBrickController = function ($scope) {
+  $scope.filter = function (tag) {
+    $scope.$emit('filter', tag);
+  };
+};
+
 var itemBrick = function () {
   return {
     restrict: 'A',
     scope: {
       item: '=itemBrick'
     },
-    template: require('../../templates/items/brick.jade')
+    template: require('../../templates/items/brick.jade'),
+    controller: ['$scope', itemBrickController]
   };
 };
 
